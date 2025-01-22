37c22f1516735c16a510a7d85c1b0d88510c048d
acmerev <acmerev@gmail.com>
2025-01-21 19:20:50 -0600

    proyect for alura and oracle of challenge Foro Hub

---
+++ src/main/java/mx/com/challengeforohub/ChallengeforohubApplication.java
@@ -0,0 +1,13 @@
+package mx.com.challengeforohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeforohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeforohubApplication.class, args);
+	}
+
+}
